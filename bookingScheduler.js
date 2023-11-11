f79e36a967b4e376931bd532b987ba9399765e21
Mridul Dhiman <dhimanmridul91@gmail.com>
2023-11-11 20:43:08 +0530

    [Feat] Initial Cron Job for checked Out bookings

--- bookingScheduler.js
+++ bookingScheduler.js
@@ -2,47 +2,49 @@ const cron = require("node-cron");
 const Booking = require("./models/booking/booking");
 const guestHouse = require("./models/guestHouse");
 
+const cronJobForBookingCompletion =  () => {
+    cron.schedule("0 0 * * *", async () => {
 
 
-// schedule every day
-cron.schedule("0 0 * * *", async () => {
-
-
-    //1. update the status to Checked Out
-    //2. change the guestHouse to default
-    const currentDate = new Date();
-    try{
-   
-    const checkedOutBookings = await Booking.find({status:"approved", endDate: {$lte: currentDate}});
-
-for(const booking of checkedOutBookings) {
-    await Booking.updateOne({
-        _id: booking._id
-    }, {
-            status: "checkedOut"
-    });
+        //1. update the status to Checked Out
+        //2. change the guestHouse to default
+        const currentDate = new Date();
+        try{
+       
+        const checkedOutBookings = await Booking.find({status:"approved", endDate: {$lt: currentDate}});
     
-  const guestHouses = booking.guestHouseAllotted;
-  const rooms = booking.roomAllotted;
-
-  for(let i = 0;i<2;i++) {
-    const setObj = {};
-    setObj[`rooms.${rooms[i]-1}`] = false;
-         await guestHouse.updateOne({
-            guestHouseId: guestHouses[i]
-         }, {
-              $set : setObj
-         });
-  }
-
-}
+    for(const booking of checkedOutBookings) {
+        await Booking.updateOne({
+            _id: booking._id
+        }, {
+                status: "checkedOut"
+        });
+        
+      const guestHouses = booking.guestHouseAllotted;
+      const rooms = booking.roomsAllotted;
     
-}
+      for(let i = 0;i<rooms.length;i++) {
+        const setObj = {};
+        setObj[`rooms.${rooms[i]-1}`] = false;
+             await guestHouse.updateOne({
+                guestHouseId: guestHouses
+             }, {
+                  $set : setObj
+             });
+      }
+    } 
 
-    catch(err) {
-        console.log({message: err.message});
+    await axios.get("https://guest-house-back.onrender.com/calendar/create");
+        
     }
+        catch(err) {
+            console.log({message: err.message});
+        } 
+    })
+}
 
 
+module.exports = cronJobForBookingCompletion;
+// schedule every day
 
-})
+
