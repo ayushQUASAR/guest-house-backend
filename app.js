c6ad179b252f35c674c67b441f539d8ba959202e
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-01-25 00:35:09 +0530

    [Feat] Backend deployed on vercel

---
+++ app.js
@@ -0,0 +1,85 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require('cors');
+const session = require("express-session")
+const app = express();
+
+
+//configuring routes 
+const registerRoute = require("./routes/userRegistration");
+const approveRegistrationRoute = require("./routes/approveRegistration");
+const usersRoute = require("./routes/users");
+const loginRoute = require("./routes/login");
+const referenceRoutes = require("./routes/references");
+const emailRoute = require("./routes/email")
+const bookingRoute = require("./routes/booking/booking");
+const bookingApprovalRoute = require("./routes/booking/bookingApproval");
+const guestHouseRoute = require("./routes/guestHouse")
+const refundRoute = require("./routes/refund/refund");
+
+app.enable('trust proxy');
+// const origins = ['http://localhost:5173',"https://guest-house-system-eight.vercel.app/"];
+app.use(cors({
+    origin: "*",
+    // default: "http://localhost:5173",
+    credentials: true,
+}));
+//body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'your secret key',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        sameSite: 'lax'
+    }
+}));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+// for session checking
+app.get('/check-session', (req, res) => {
+    // console.log(req.session);
+    if (req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user, isAdmin: req.session.isAdmin });
+        console.log("loggedIn is true");
+    } else {
+        res.send({ loggedIn: false });
+        console.log("loggedIn is false");
+    }
+});
+
+// for logout
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            res.send({ message: 'Error logging out' });
+        } else {
+            res.send({ message: 'Logged out successfully' });
+        }
+    });
+});
+
+//listening on port 3000
+app.use("/register", registerRoute);
+app.use("/admin/approveRegistration", approveRegistrationRoute);
+app.use("/users", usersRoute);
+app.use("/login", loginRoute);
+app.use("/references", referenceRoutes);
+app.use("/email", emailRoute);
+//booking routes
+app.use("/booking", bookingRoute);
+app.use("/admin/bookingApproval", bookingApprovalRoute);
+app.use("/guestHouse", guestHouseRoute);
+app.use("/refund", refundRoute);
+// app.use("/check-session", sessionRoute);
+
+app.use("/images", require("./routes/images"));
+app.use("/calendar", require("./routes/calendar"));
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const express = require("express");
-const session = require('express-session');
+
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const multer = require("multer");
 dotenv.config();
-const cors = require("cors");
 
 
-//configuring routes 
-const registerRoute = require("./routes/userRegistration");
-const approveRegistrationRoute = require("./routes/approveRegistration");
-const usersRoute = require("./routes/users");
-const loginRoute = require("./routes/login");
-const referenceRoutes = require("./routes/references");
-const emailRoute = require("./routes/email")
-const bookingRoute = require("./routes/booking/booking");
-const bookingApprovalRoute = require("./routes/booking/bookingApproval");
-const guestHouseRoute = require("./routes/guestHouse")
-const refundRoute = require("./routes/refund/refund");
+
+const app = require('./app');
 // const sessionRoute = require('./routes/session');
 
 
@@ -29,8 +17,7 @@ const cronJobForPaymentDeadline = require("./paymentDeadlineScheduler");
 cronJobForPaymentDeadline();
 
 //configuring packages
-const app = express();
-app.enable('trust proxy');
+// const app = express();
 // mongoose.connect("mongodb+srv://user:user@cluster0.5rmy7ke.mongodb.net/guest-house");
 mongoose.connect('mongodb+srv://user:user@cluster0.uunf6ts.mongodb.net/?retryWrites=true&w=majority');
 // mongoose.connect("mongodb://127.0.0.1:27017/guestHouse");
@@ -45,71 +32,9 @@ db.once('open', () => {
 db.on("error", (err) => {
     console.log({ database_message: err.message });
 });
-const port = process.env.PORT || 3000;
-const origins = ['http://localhost:5173',"https://guest-house-system.vercel.app/"];
-app.use(cors({
-    origin: origins,
-    default: "http://localhost:5173",
-    credentials: true,
-}));
-//body parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
 
-app.use(session({
-    secret: 'your secret key',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        secure: false,
-        sameSite: 'lax'
-    }
-}));
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
-
-// for session checking
-app.get('/check-session', (req, res) => {
-    // console.log(req.session);
-    if (req.session.user) {
-        res.send({ loggedIn: true, user: req.session.user, isAdmin: req.session.isAdmin });
-        console.log("loggedIn is true");
-    } else {
-        res.send({ loggedIn: false });
-        console.log("loggedIn is false");
-    }
-});
-
-// for logout
-app.get('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            res.send({ message: 'Error logging out' });
-        } else {
-            res.send({ message: 'Logged out successfully' });
-        }
-    });
-});
-
-//listening on port 3000
-app.use("/register", registerRoute);
-app.use("/admin/approveRegistration", approveRegistrationRoute);
-app.use("/users", usersRoute);
-app.use("/login", loginRoute);
-app.use("/references", referenceRoutes);
-app.use("/email", emailRoute);
-//booking routes
-app.use("/booking", bookingRoute);
-app.use("/admin/bookingApproval", bookingApprovalRoute);
-app.use("/guestHouse", guestHouseRoute);
-app.use("/refund", refundRoute);
-// app.use("/check-session", sessionRoute);
-
-app.use("/images", require("./routes/images"));
-app.use("/calendar", require("./routes/calendar"));
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
