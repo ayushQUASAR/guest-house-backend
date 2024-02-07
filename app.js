b7991252329d71371549921073038ad032702cc6
a-singh09 <a_singh09@yahoo.com>
2024-02-08 01:34:59 +0530

    Added payments routes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const bookingRoute = require("./routes/booking/booking");
 const bookingApprovalRoute = require("./routes/booking/bookingApproval");
 const guestHouseRoute = require("./routes/guestHouse")
 const refundRoute = require("./routes/refund/refund");
+const paymentRoute = require("./routes/payment/payment");
 
 app.enable('trust proxy');
 // const origins = ['http://localhost:5173',"https://guest-house-system-eight.vercel.app/"];
@@ -78,6 +79,7 @@ app.use("/booking", bookingRoute);
 app.use("/admin/bookingApproval", bookingApprovalRoute);
 app.use("/guestHouse", guestHouseRoute);
 app.use("/refund", refundRoute);
+app.use("/payments", paymentRoute);
 // app.use("/check-session", sessionRoute);
 
 app.use("/images", require("./routes/images"));
