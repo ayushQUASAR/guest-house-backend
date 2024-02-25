a235c24db194906ca21cb4e1699e55db5196930f
a-singh09 <a_singh09@yahoo.com>
2024-02-25 14:45:08 +0530

    added api routes for reports section

--- routes/booking/booking.js
+++ routes/booking/booking.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const express  = require("express");
+const express = require("express");
 const axios = require("axios")
 
 
@@ -14,84 +14,107 @@ router.use("/register", require("./bookingForm"));
 
 
 // GET all the booking
-router.get("/", async (req,res) => {
-      try {
-         const bookings = await Booking.find({});
+router.get("/", async (req, res) => {
+  try {
+    const bookings = await Booking.find({});
 
-         res.status(200).json(bookings);
-      }
-      catch(err) {
-        console.log(err.message);
-        res.status(500).json({message: err.message})
-      }
+    res.status(200).json(bookings);
+  }
+  catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message })
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const bookings = await Booking.find({});
+
+    res.status(200).json(bookings);
+  }
+  catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message })
+  }
 });
 
 
 // GET bookings by ID 
-router.get("/:id", async (req,res) => {
-    const id = req.params.id;
-try {
-   const booking = await Booking.find({_id: id});
-    
-   res.status(200).json(booking);
-   
-}
-catch(err) {
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const booking = await Booking.find({ _id: id });
+
+    res.status(200).json(booking);
+
+  }
+  catch (err) {
     console.log(err.message);
-    res.status(500).json({message: err.message})
+    res.status(500).json({ message: err.message })
   }
 });
 
 
 // GET pending or registered or rejected booking 
-router.get('/approved/:approvalType', async (req,res) => {
-    const approvalType = req.params.approvalType;
+router.get('/approved/:approvalType', async (req, res) => {
+  const approvalType = req.params.approvalType;
 
-    const possibleBookingOptions = ['pending', 'approved', "cancelled", "rejected"];
-try{
-    if(approvalType === 'pending' || approvalType === 'approved' || approvalType === 'cancelled' || approvalType === 'rejected') {
-        if(approvalType === 'approved') {
-          if(req.query) {
-            const {guestHouse} = req.query;
-           const bookings =   await Booking.find({
+  const possibleBookingOptions = ['pending', 'approved', "cancelled", "rejected", "upcoming", "past"];
+  try {
+    if (approvalType === 'pending' || approvalType === 'approved' || approvalType === 'cancelled' || approvalType === 'rejected' || approvalType === 'upcoming' || approvalType === 'past') {
+      if (approvalType === 'approved') {
+        if (req.query) {
+          const { guestHouse } = req.query;
+          const bookings = await Booking.find({
             status: "approved",
-             guestHouseSelected: guestHouse
-            },  {startDate: 1, endDate: 1, name: 1, email: 1, roomsAllotted: 1 });
-          
-           const finalBooking = [];
-           bookings.forEach((booking) => {
-            for(let roomId of booking.roomsAllotted) {
-              let newBooking  = {
+            guestHouseSelected: guestHouse
+          }, { startDate: 1, endDate: 1, name: 1, email: 1, roomsAllotted: 1 });
+
+          const finalBooking = [];
+          bookings.forEach((booking) => {
+            for (let roomId of booking.roomsAllotted) {
+              let newBooking = {
                 checkInDate: booking.startDate,
                 checkOutDate: booking.endDate,
                 name: booking.name,
                 email: booking.email,
-                  roomId
+                roomId
               }
 
               finalBooking.push(newBooking);
             }
-            
-           })
 
-           return res.status(200).json(finalBooking);
-                         
-          }
-         
+          })
+
+          return res.status(200).json(finalBooking);
+
         }
 
+      }
 
-        const booking = await Booking.find({status: approvalType});
-        res.status(200).json(booking);  
-  }
-else {
-  throw new Error("arrival type not allowed");
-}
-  }
-    catch(err) {
-      console.log(err.message);
-      res.status(500).json({message: err.message})
+      else if (approvalType === 'upcoming') {
+        const bookings = await Booking.find({ status: 'approved', startDate: { $gte: new Date() } });
+        res.status(200).json(bookings);
+      }
+
+      else if (approvalType === 'past') {
+        const bookings = await Booking.find({ status: 'checkedOut', endDate: { $lt: new Date() } });
+        res.status(200).json(bookings);
+      }
+
+      else {
+        const booking = await Booking.find({ status: approvalType });
+        res.status(200).json(booking);
+      }
     }
+    else {
+      throw new Error("arrival type not allowed");
+    }
+  }
+  catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message })
+  }
 });
 
 
@@ -101,81 +124,81 @@ else {
 
 // DELETE FOR (BOOKING CANCELLATION): delete the booking and remove from booking history
 // Changing status to cancelled
-router.post("/:id", async (req,res) => {
+router.post("/:id", async (req, res) => {
   const id = req.params.id;
-try {
+  try {
 
-  // booking cancelled
-  const y = await Booking.findByIdAndUpdate(id, {
-    status: 'cancelled'
-  }, {
-    new: true
-  });
-
-  
-  // make booking status cancelled
-
-console.log(y);
-// update the info in guesthouse collection
-
-
-
-if(y.roomsAllotted.length > 0) {
-  const rooms = y.roomsAllotted;
-  const guestHouses = y.guestHouseAllotted;
-  
-  for(let i = 0;i<rooms.length;i++) {
-    const incObject = {};
-    incObject[`rooms.${rooms[i]-1}`] = false;
-    await guestHouse.updateOne({
-      guestHouseId: guestHouses
+    // booking cancelled
+    const y = await Booking.findByIdAndUpdate(id, {
+      status: 'cancelled'
     }, {
-      $set:  incObject
+      new: true
     });
+
+
+    // make booking status cancelled
+
+    console.log(y);
+    // update the info in guesthouse collection
+
+
+
+    if (y.roomsAllotted.length > 0) {
+      const rooms = y.roomsAllotted;
+      const guestHouses = y.guestHouseAllotted;
+
+      for (let i = 0; i < rooms.length; i++) {
+        const incObject = {};
+        incObject[`rooms.${rooms[i] - 1}`] = false;
+        await guestHouse.updateOne({
+          guestHouseId: guestHouses
+        }, {
+          $set: incObject
+        });
+      }
+    }
+
+
+
+
+    // update the Registered user's booking history if booking cancelled
+    // if(!y.roomBooker.isAdmin) {
+    //   const registeredUsers = await RegisteredUser.find({}).populate('user');
+    //   const user =   registeredUsers.filter((user) => user.user.email === y.roomBooker.email);
+
+
+    //   await RegisteredUser.updateOne(
+    //     {_id: user[0]._id}, {
+    //       $pull: {
+    //         bookingHistory: id,
+    //       },
+    //     });
+    //   }
+
+
+
+    res.json({ message: "Booking Cancelled Successfully..." });
+
+
   }
-}
 
-
-
-
-// update the Registered user's booking history if booking cancelled
-// if(!y.roomBooker.isAdmin) {
-//   const registeredUsers = await RegisteredUser.find({}).populate('user');
-//   const user =   registeredUsers.filter((user) => user.user.email === y.roomBooker.email);
-  
-  
-//   await RegisteredUser.updateOne(
-//     {_id: user[0]._id}, {
-//       $pull: {
-//         bookingHistory: id,
-//       },
-//     });
-//   }
-
-  
-  
-  res.json({message: "Booking Cancelled Successfully..."});
-
-
-}
-
-catch(err) {
-  console.log({message: err.message});
-  res.status(500).json({message: err.message})
-}
+  catch (err) {
+    console.log({ message: err.message });
+    res.status(500).json({ message: err.message })
+  }
 });
 
 
 
 router.delete("/", async (req, res) => {
-try {
-await Booking.deleteMany({});
+  try {
+    await Booking.deleteMany({});
 
-res.json({message: "All Bookings deleted Successfully"})
-}
-catch(err) {
-res.json({message: err.message})
-}
+    res.json({ message: "All Bookings deleted Successfully" })
+  }
+  catch (err) {
+    res.json({ message: err.message })
+  }
 })
 
 module.exports = router;
